1b5c8e10465af1606253d9dad8515f2bd62a23a2
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-10 15:50:10 -0400

    more img, added hide/active state

--- client/src/pages/SkillsPageContent/SkillsPageContent.styles.jsx
+++ client/src/pages/SkillsPageContent/SkillsPageContent.styles.jsx
@@ -2,30 +2,53 @@ import styled from 'styled-components'
 
 import { cssIndex, device } from '../../styles/css/utils.styles.js'
 
-const { tabletMM } = device
+const { tabletMM, mobileS } = device
 
 export const SkillsPageContentContainer = styled.div`
   ${cssIndex.flexCenterCol}
 
   ul {
+    display: none;
     max-width: 85%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    grid-auto-rows: 300px;
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    grid-auto-rows: 250px;
+    grid-row-gap: 4rem;
+    grid-column-gap: 3rem;
+    justify-content: center;
+    justify-items: center;
     li {
+      ${cssIndex.flexCenterCol}
       width: 100%;
       img {
-        width: 100%;
+        max-width: 100%;
+        max-height: 100%;
+
+        &.aws-img {
+          max-width: 120%;
+          max-height: 120%;
+        }
       }
+    }
+
+    &.active {
+      display: grid;
     }
   }
 
   ${tabletMM} {
     padding-top: 3rem;
     ul {
-      grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
       grid-auto-rows: 200px;
-      grid-row-gap: 4rem;
+      grid-row-gap: 5rem;
+    }
+  }
+
+  ${mobileS} {
+    ul {
+      grid-template-columns: 190px;
+      grid-template-rows: 180px;
+      grid-row-gap: 3rem;
     }
   }
 `
