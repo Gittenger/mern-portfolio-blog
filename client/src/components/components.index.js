064cdc31d8866e8b24b4387130cde1bf0f927da3
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-15 20:47:27 -0400

    fixed for imports

--- client/src/components/components.index.js
+++ client/src/components/components.index.js
@@ -1,35 +1,37 @@
 import AdminDashboard from './Admin/AdminDashboard/AdminDashboard.component.jsx'
 import AdminMenu from './Admin/AdminMenu/AdminMenu.component.jsx'
 import AdminRoute from './Admin/AdminRoute/AdminRoute.component.jsx'
-import BlogPost from './General/BlogPost/BlogPost.component.jsx'
-import ContactForm from './General/ContactForm/ContactForm.component.jsx'
 import ChangePassword from './Admin/ChangePassword/ChangePassword.component.jsx'
-import Checkbox from './General/Checkbox/Checkbox.component.jsx'
-import Footer from './Core/Footer/Footer.component.jsx'
 import ForgotPassword from './Admin/ForgotPassword/ForgotPassword.component.jsx'
+import ResetPassword from './Admin/ResetPassword/ResetPassword.component.jsx'
+import Card from './General/Card/Card.component.jsx'
+import Footer from './Core/Footer/Footer.component.jsx'
 import Layout from './Core/Layout/Layout.component.jsx'
 import NavDesk from './Core/NavDesk/NavDesk.component.jsx'
 import NavList from './Core/NavList/NavList.component.jsx'
 import NavMobile from './Core/NavMobile/NavMobile.component.jsx'
-import ResetPassword from './Admin/ResetPassword/ResetPassword.component.jsx'
 import ToggleNavButton from './Core/ToggleNavButton/ToggleNavButton.component.jsx'
+import BlogPost from './General/BlogPost/BlogPost.component.jsx'
+import Checkbox from './General/Checkbox/Checkbox.component.jsx'
+import ContactForm from './General/ContactForm/ContactForm.component.jsx'
 import TComp from './typography/typography.components.js'
 export const CIndex = {
 	AdminDashboard,
 	AdminMenu,
 	AdminRoute,
-	BlogPost,
-	ContactForm,
 	ChangePassword,
-	Checkbox,
-	Footer,
 	ForgotPassword,
+	ResetPassword,
+	Card,
+	Footer,
 	Layout,
 	NavDesk,
 	NavList,
 	NavMobile,
-	ResetPassword,
 	ToggleNavButton,
+	BlogPost,
+	Checkbox,
+	ContactForm,
 	TComp,
 }
 export default CIndex
