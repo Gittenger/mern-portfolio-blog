6b016b5a24544cc98fd6696e5f0456ac0fd67c54
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-25 04:36:06 -0400

    ScrollToTop implementation

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import CIndex from './components/components.index.js'
+import ScrollToTop from './utils/ScrollToTop.js'
 
 import {
   HomePage,
@@ -27,32 +28,43 @@ const Routes = () => {
   } = CIndex
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/login" exact component={LoginPage} />
-        <Route path="/portfolio" exact component={PortfolioPage} />
-        <Route
-          path="/portfolio/:projectName"
-          exact
-          component={PortfolioDetailsPage}
-        />
-        <Route path="/addProject" exact component={AddProject} />
-        <Route path="/contact" exact component={ContactPage} />
-        <Route path="/skills" exact component={SkillsPage} />
-        <Route path="/resume" exact component={ResumePage} />
-        <Route path="/build" exact component={BuildPage} />
-        <Route path="/blog" exact component={BlogPage} />
-        <Route path="/blog/posts" exact component={BlogPage} />
-        <Route path="/blog/posts/:slug" exact component={BlogDetailsPage} />
-        <AdminRoute path="/admin" exact component={AdminDashboard} />
-        <AdminRoute path="/forgot-password" exact component={ForgotPassword} />
-        <AdminRoute path="/change-password" exact component={ChangePassword} />
-        <AdminRoute
-          path="/reset-password/:token"
-          exact
-          component={ResetPassword}
-        />
-      </Switch>
+      <Fragment>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/portfolio" exact component={PortfolioPage} />
+          <Route
+            path="/portfolio/:projectName"
+            exact
+            component={PortfolioDetailsPage}
+          />
+          <Route path="/addProject" exact component={AddProject} />
+          <Route path="/contact" exact component={ContactPage} />
+          <Route path="/skills" exact component={SkillsPage} />
+          <Route path="/resume" exact component={ResumePage} />
+          <Route path="/build" exact component={BuildPage} />
+          <Route path="/blog" exact component={BlogPage} />
+          <Route path="/blog/posts" exact component={BlogPage} />
+          <Route path="/blog/posts/:slug" exact component={BlogDetailsPage} />
+          <AdminRoute path="/admin" exact component={AdminDashboard} />
+          <AdminRoute
+            path="/forgot-password"
+            exact
+            component={ForgotPassword}
+          />
+          <AdminRoute
+            path="/change-password"
+            exact
+            component={ChangePassword}
+          />
+          <AdminRoute
+            path="/reset-password/:token"
+            exact
+            component={ResetPassword}
+          />
+        </Switch>
+      </Fragment>
     </BrowserRouter>
   )
 }
