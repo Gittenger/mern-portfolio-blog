b1cd51443728145ed77f95368330565dfdefcc1a
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-25 04:36:35 -0400

    improved Home page w/ styles

--- client/src/pages/Home/Home.component.jsx
+++ client/src/pages/Home/Home.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import Images from '../../assets/img/img-index'
@@ -13,53 +13,94 @@ const Home = () => {
 		TComp: { P, PSmall },
 	} = CIndex
 
-	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [])
-
 	return (
 		<HomePageContainer>
-			<P>
-				Hi. I'm John. I'm a software developer with a passion for learning and a
-				bold outlook towards the future.
-			</P>
-			<PSmall>
-				I have over three years of coding experience and I specialize in web
-				technologies. My stack of choice is MERN.
-			</PSmall>
-			<img src={ProfileImg} />
-			<PSmall>
-				This web page was built using a MERN stack and deployed via cloud
-				computing with AWS. I built a framework on top of React to help me
-				quickly develop this UI and any other UI's in the future.
-			</PSmall>
-			<P>My skills include but are not limited to:</P>
+			<div className="intro-text text-box">
+				<P className="leading">Hi! I'm John.</P>
+				<P className="leading-sub">
+					I'm a software developer with a passion for learning and a bold
+					outlook towards the future.
+				</P>
+				<PSmall className="leading-subTwo">
+					I have over three years of coding experience and I specialize in web
+					technologies. My stack of choice is MERN.
+				</PSmall>
+			</div>
+			<div className="img-box">
+				<img src={ProfileImg} />
+			</div>
+			<div className="text-box page-desc">
+				<PSmall>
+					This web page was built using a MERN stack and deployed via cloud
+					computing with AWS. I built a framework on top of React to help me
+					quickly develop this UI and any other UI's in the future.
+				</PSmall>
+			</div>
+			<div className="text-box list-header">
+				<P>My skills include but are not limited to...</P>
+			</div>
 			<ul>
-				<li>React</li>
-				<li>NodeJS</li>
-				<li>Sass/CSS</li>
-				<li>Linux/Unix</li>
-				<li>Vim</li>
-				<li>Cloud computing</li>
-				<li>Git</li>
-				<li>UI Design</li>
-				<li>Python</li>
-				<li>GraphQL</li>
-				<li>Redux</li>
-				<li>Google Firebase</li>
+				<li>
+					<PSmall>React</PSmall>
+				</li>
+				<li>
+					<PSmall>NodeJS</PSmall>
+				</li>
+				<li>
+					<PSmall>Sass/CSS</PSmall>
+				</li>
+				<li>
+					<PSmall>Linux/Unix</PSmall>
+				</li>
+				<li>
+					<PSmall>Vim</PSmall>
+				</li>
+				<li>
+					<PSmall>Cloud computing</PSmall>
+				</li>
+				<li>
+					<PSmall>Git</PSmall>
+				</li>
+				<li>
+					<PSmall>UI Design</PSmall>
+				</li>
+				<li>
+					<PSmall>Python</PSmall>
+				</li>
+				<li>
+					<PSmall>GraphQL</PSmall>
+				</li>
+				<li>
+					<PSmall>Redux</PSmall>
+				</li>
+				<li>
+					<PSmall>Google Firebase</PSmall>
+				</li>
 			</ul>
-			<PSmall>
-				Over my years of study I have tried lots of things. I enjoy web
-				development a lot, and I am following my passion for it into my career.
-			</PSmall>
-			<PSmall>Here is a sampling of the work I've done:</PSmall>
-			{/* Carousel of projects here */}
-			{/* Link to project details page as well */}
-			<P>
-				Right now I am ready to put my skills to good use and am currently
-				looking for an employer with whom I'll be a good fit.
-			</P>
-			<PSmall>I'd love to chat with you. Please, drop me a line:</PSmall>
+			<div className="text-box project-desc">
+				<PSmall>
+					Over my years of study I have tried lots of things in an effort to
+					broaden my skill set. I enjoy web development a lot, and I am
+					following my self-directed passion for it into my career.
+				</PSmall>
+				<PSmall>Here is a sampling of the work I've done:</PSmall>
+			</div>
+			<div className="projects-sample">
+				<div className="project-link">
+					<Link to="/portfolio/my-first-project">Project One</Link>
+				</div>
+
+				<div className="project-link">
+					<Link to="/portfolio/project-two">Project Two</Link>
+				</div>
+			</div>
+			<div className="text-box cta">
+				<P>
+					Right now I am ready to put my skills to good use and am currently
+					looking for an employer with whom I'll be a good fit.
+				</P>
+				<PSmall>I'd love to chat with you. Please, drop me a line:</PSmall>
+			</div>
 			<Link to="/contact">Contact Me</Link>
 		</HomePageContainer>
 	)
--- client/src/pages/Home/Home.styles.jsx
+++ client/src/pages/Home/Home.styles.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-import { cssIndex } from '../../styles/css/utils.styles.js'
+import { cssIndex, device, deviceMin } from '../../styles/css/utils.styles.js'
+const { tabletMM } = device
+const { minLaptopS } = deviceMin
 
 export const HomePageContainer = styled.div`
 	${cssIndex.flexCenter}
@@ -8,4 +10,117 @@ export const HomePageContainer = styled.div`
 	width: 100%;
 	background: none;
 	padding: 0 0 10rem;
+
+	// all text-boxes
+	.text-box {
+		${cssIndex.flexCenter}
+		flex-direction: column;
+
+		& > p {
+			max-width: 90%;
+		}
+	}
+
+	// intro
+	& > .intro-text {
+		.leading {
+			margin-bottom: 0;
+			--scale: 1.3;
+
+			&-sub {
+				margin-bottom: 1.5rem;
+			}
+		}
+	}
+
+	// img
+	& > .img-box {
+		max-width: 95%;
+		margin-bottom: 1.8rem;
+
+		img {
+			width: 100%;
+			border-radius: 30px;
+		}
+	}
+
+	// page-desc
+	& > .page-desc > p {
+		margin-bottom: 2rem;
+	}
+
+	// list-header
+	& > .list-header {
+		width: 100%;
+		& > p {
+			width: 100%;
+			margin-bottom: 1.5rem;
+		}
+	}
+
+	// List
+	& > ul {
+		margin-bottom: 6rem;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+		grid-row-gap: 1.2rem;
+		grid-column-gap: 4rem;
+		grid-auto-rows: 40px;
+		width: 80%;
+
+		li {
+			${cssIndex.flexCenterCol}
+
+			p {
+				margin-bottom: 0;
+				font-style: italic;
+				font-weight: bold;
+			}
+		}
+
+		${minLaptopS} {
+			grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+			li {
+				p {
+					white-space: nowrap;
+				}
+			}
+		}
+	}
+
+	// project-desc
+	& > .project-desc {
+		& > p:first-child {
+			margin-bottom: 3rem;
+		}
+
+		& > p:last-child {
+			margin-bottom: 2rem;
+		}
+	}
+
+	// projects
+	& > .projects-sample {
+		${cssIndex.flexCenter}
+		align-items: space-evenly;
+		margin-bottom: 4rem;
+
+		& > .project-link {
+			background: red;
+			border-radius: 50%;
+			width: 100px;
+			height: 100px;
+			margin: 0 2rem;
+
+			a {
+				${cssIndex.flexCenterCol}
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+
+	${tabletMM} {
+		padding: 4rem 0 5rem;
+	}
 `
