deb13e5dd2ea4d1fff247e93de79a41924115e29
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-25 02:42:01 -0400

    updated card data

--- client/src/pages/Skills/Skills.component.jsx
+++ client/src/pages/Skills/Skills.component.jsx
@@ -10,26 +10,34 @@ const Skills = () => {
 	return (
 		<SkillsPageContentContainer>
 			{/* Have a setup section to explain my development environment */}
+			<div className="page-title">
+				<h1>Skills</h1>
+			</div>
 			<SkillCardsContainer>
 				<ul>
 					<li>
 						<SkillCard
 							name="NodeJS"
-							desc="I am a competent nodeJS developer"
+							desc="I am very experienced at building web applications on the Node runtime. I am extremely familiar with all the basics of its ins and outs and can quickly set up and configure new applications to be powered with a NodeJS back-end."
 							bullet={[
-								'NPM',
-								'Building robust back-ends using tools such as Express',
+								'Using NPM to find and manage all necessary tools and plugins',
+								'Building robust back-ends using tools and libraries such as Express or Adonis',
+								"Creating API's to run server side to be consumed by client",
+								'Scripting production commands to run application on production server such as inside a cloud-based VPS or container',
+								'Custom Node scripts to be executed at the command line for managing read/write operations on file system',
 							]}
-							competency="High"
 							img="nodejs"
 						/>
 					</li>
 					<li>
 						<SkillCard
 							name="Git"
-							desc="I am competent at using Git"
-							bullet={['Git version control', 'Use it daily']}
-							competency="High"
+							desc="I am highly competent at using git to manage projects. I use it on every personal project and have gotten better over the years at using it efficiently."
+							bullet={[
+								'All basics -- branching, pulling, pushing, merging, sending pull requests, comparing with diff',
+								'I use it daily and utilize shell aliases to issue almost all git commands with only a few keystrokes',
+								'Small commits for better traceability',
+							]}
 							img="git"
 						/>
 					</li>
