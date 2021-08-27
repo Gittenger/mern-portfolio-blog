6aba8b1a64ecf3485050507f859ab0a431eee408
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-27 14:04:54 -0400

    checkForUpdate client implement working basic

--- client/src/pages/Skills/Skills.component.jsx
+++ client/src/pages/Skills/Skills.component.jsx
@@ -10,22 +10,48 @@ import UrlsContext from '../../contexts/UrlsContext.js'
 
 const Skills = () => {
 	const { SkillCard } = CIndex
-	const [values, setValues] = useState([
-		{ name: '', desc: '', bullet: [], img: '', years: '' },
-	])
+	const [values, setValues] = useState({
+		data: [{ name: '', desc: '', bullet: [], img: '', years: '' }],
+		timeStamp: '',
+	})
+	const [shouldFetchNewApi, setShouldFetchNewApi] = useState(false)
 	const { urls, setUrlsContext } = useContext(UrlsContext)
 
 	const url = `${process.env.REACT_APP_API}/skills`
+	const checkServerUrl = `${process.env.REACT_APP_API}/checkForUpdate`
 
 	useEffect(() => {
-		if (localStorage.getItem(url)) {
+		fetch(checkServerUrl, {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(res => res.json())
+			.then(res => {
+				if (JSON.parse(localStorage.getItem(url))) {
+					if (JSON.parse(localStorage.getItem(url)).timeStamp < res.lastUpdated) {
+						setShouldFetchNewApi(true)
+					}
+				}
+				if (urls[url]) {
+					if (urls[url].timeStamp && urls[url].timeStamp < res.lastUpdated) {
+						setShouldFetchNewApi(true)
+					}
+				}
+			})
+	}, [])
+
+	useEffect(() => {
+		if (localStorage.getItem(url) && shouldFetchNewApi == false) {
 			console.log('getting from local')
 			setValues(JSON.parse(localStorage.getItem(url)))
 			setUrlsContext(
 				url,
 				localStorage.getItem(JSON.parse(localStorage.getItem(url)))
 			)
-		} else if (urls[url]) {
+		} else if (urls[url] && shouldFetchNewApi == false) {
 			console.log('getting from context')
 			setValues(urls[url])
 		} else {
@@ -39,13 +65,14 @@ const Skills = () => {
 			})
 				.then(res => res.json())
 				.then(res => {
-					localStorage.setItem(url, JSON.stringify(res.skills))
-					setUrlsContext(url, res.skills)
-					setValues(res.skills)
+					const dataObj = { timeStamp: new Date(), data: res.skills }
+					localStorage.setItem(url, JSON.stringify(dataObj))
+					setUrlsContext(url, dataObj)
+					setValues(dataObj)
 				})
 				.catch(err => console.error(err))
 		}
-	}, [])
+	}, [shouldFetchNewApi])
 
 	return (
 		<SkillsPageContentContainer>
@@ -55,7 +82,7 @@ const Skills = () => {
 			</div>
 			<SkillCardsContainer>
 				<ul>
-					{values.map(({ ...props }, i) => (
+					{values.data.map(({ ...props }, i) => (
 						<li key={i}>
 							<SkillCard {...props} />
 						</li>
