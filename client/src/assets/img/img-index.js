feabee9f25957ee5f80cb8004622fbc3b3b76b82
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-08-30 13:20:42 -0400

    updated img asset naming/indexing

--- client/src/assets/img/img-index.js
+++ client/src/assets/img/img-index.js
@@ -1,9 +1,27 @@
-import ImgOne from './img-1.jpeg'
-import ImgTwo from './img-2.jpeg'
-import ImgTnree from './img-3.jpeg'
-import ProfileImg from './profile.jpeg'
+import node from './skills/node.png'
+import aws from './skills/aws.png'
+import html from './skills/html.png'
+import redux from './skills/redux.png'
+import css from './skills/css.png'
+import gatsby from './skills/gatsby.png'
+import python from './skills/python.png'
+import git from './skills/git.png'
+import figma from './skills/svg/figma.svg'
+import firebase from './skills/svg/firebase.svg'
+import graphql from './skills/svg/graphql.svg'
+import linux from './skills/svg/linux.svg'
+import nextjs from './skills/svg/nextjs.svg'
+import pug from './skills/svg/pug.svg'
+import react from './skills/svg/react.svg'
+import vim from './skills/svg/vim.svg'
+import profile from './profile.jpeg'
 
-export const Images = { ProfileImg }
-export const LocalImages = [ImgOne, ImgTwo, ImgTnree]
+export const Images = {
+	skills: {
+		png: { node, aws, html, redux, css, gatsby, python, git },
+		svg: { figma, firebase, graphql, linux, nextjs, pug, react, vim },
+	},
+	profile,
+}
 
 export default Images
