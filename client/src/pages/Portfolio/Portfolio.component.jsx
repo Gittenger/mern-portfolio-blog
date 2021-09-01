5fc33b50533d582a25d27f6b7551360165e0aaa0
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-01 02:50:16 -0400

    implement cache for project overview

--- client/src/pages/Portfolio/Portfolio.component.jsx
+++ client/src/pages/Portfolio/Portfolio.component.jsx
@@ -1,43 +1,88 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { useShouldUpdateCache } from '../../utils/hooks.js'
 import { Link } from 'react-router-dom'
 
 import CIndex from '../../components/components.index.js'
 import { PortfolioPageContainer, PortfolioCardList } from './Portfolio.styles'
 
+import UrlsContext from '../../contexts/UrlsContext.js'
 import DesignOne from '../../assets/img/design-1.png'
-import DesignTwo from '../../assets/img/design-2.png'
-import DesignThree from '../../assets/img/design-3.png'
+// import DesignTwo from '../../assets/img/design-2.png'
+// import DesignThree from '../../assets/img/design-3.png'
 
 const Portfolio = () => {
 	const { ProjectCard } = CIndex
-	const [values, setValues] = useState([])
+	const [values, setValues] = useState({
+		data: {
+			key: {
+				name: '',
+				description: '',
+				slug: '',
+			},
+		},
+		timeStamp: '',
+	})
+	const { urls, setUrlsContext } = useContext(UrlsContext)
+
 	const url = `${process.env.REACT_APP_API}/projects`
+	const shouldFetchNewApi = useShouldUpdateCache(url)
+
+	const cachedItem = JSON.parse(localStorage.getItem(url))
 
 	useEffect(() => {
-		fetch(url, {
-			method: 'GET',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		})
-			.then(res => res.json())
-			.then(res => {
-				setValues(res.data)
+		let unmounted = false
+		if (unmounted) return
+
+		if (cachedItem && shouldFetchNewApi === false) {
+			console.log('from local')
+			if (!unmounted) {
+				setValues(cachedItem)
+			}
+			setUrlsContext(url, cachedItem)
+		} else if (urls[url] && shouldFetchNewApi === false) {
+			console.log('from context')
+			if (!unmounted) {
+				setValues(urls[url])
+			}
+		} else {
+			fetch(url, {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
 			})
-			.catch(err => console.error(err))
+				.then((res) => res.json())
+				.then((res) => {
+					const fromData = {}
+					res.data.forEach((project) => {
+						fromData[project.slug] = project
+					})
+					const dataObject = { timeStamp: new Date(), data: fromData }
+					localStorage.setItem(url, JSON.stringify(dataObject))
+					setUrlsContext(url, dataObject)
+					if (!unmounted) {
+						setValues(dataObject)
+					}
+				})
+				.catch((err) => console.error(err))
+		}
+
+		return () => {
+			unmounted = true
+		}
 	}, [url])
 
 	return (
 		<PortfolioPageContainer>
 			<Link to="/addProject">Add Project</Link>
 			<PortfolioCardList>
-				{values.map(({ name, description, slug }, i) => (
+				{Object.keys(values.data).map((project, i) => (
 					<li key={i}>
 						<ProjectCard
-							title={name}
-							description={description}
-							anchor={`portfolio/${slug}`}
+							title={values.data[project].name}
+							description={values.data[project].description}
+							anchor={`portfolio/${values.data[project].slug}`}
 							coverImg={DesignOne}
 						/>
 					</li>
