9cf855404fd2873a0eb7529da8848f98ae749a7a
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-05 13:44:41 -0400

    basic styling of port.details done

--- client/src/pages/PortfolioDetails/PortfolioDetails.component.jsx
+++ client/src/pages/PortfolioDetails/PortfolioDetails.component.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { useShouldUpdateCache } from '../../utils/hooks.js'
 import { useParams } from 'react-router-dom'
+import ReactMarkdown from 'react-markdown'
+import { Prism } from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import CIndex from '../../components/components.index.js'
 import { PortfolioDetailsContainer } from './PortfolioDetails.styles'
@@ -60,17 +63,57 @@ const PortfolioDetails = () => {
 		}
 	}, [])
 
+	const renderers = {
+		code({ node, inline, className, children, ...props }) {
+			const match = /language-(\w+)/.exec(className || '')
+			return !inline && match ? (
+				<Prism
+					style={dracula}
+					language={match[1]}
+					PreTag="div"
+					children={String(children).replace(/\n$/, '')}
+					{...props}
+				/>
+			) : inline ? (
+				<code className={className} {...props}>
+					{children}
+				</code>
+			) : (
+				<Prism
+					style={dracula}
+					language="shell"
+					children={String(children).replace(/\n$/, '')}
+					{...props}
+				/>
+			)
+		},
+		p({ ...props }) {
+			return <PSmall {...props} />
+		},
+		h1({ ...props }) {
+			return <H1 {...props} />
+		},
+		h2({ ...props }) {
+			return <H2 {...props} />
+		},
+		h3({ ...props }) {
+			return <H3 {...props} />
+		},
+	}
+
 	const { name, descriptionLong, link, techStack } = values
 
 	const {
-		TComp: { PSmall },
+		TComp: { PSmall, H1, H3, H2 },
 		SkillImage,
 	} = CIndex
 
 	return (
 		<PortfolioDetailsContainer>
 			<PSmall>{name}</PSmall>
-			<PSmall>{descriptionLong}</PSmall>
+			<div className="description-long">
+				<ReactMarkdown components={renderers} children={descriptionLong} />
+			</div>
 			<a href={link}>Link to project</a>
 			<ul>
 				{techStack.map((el, i) => {
--- client/src/pages/PortfolioDetails/PortfolioDetails.styles.jsx
+++ client/src/pages/PortfolioDetails/PortfolioDetails.styles.jsx
@@ -4,10 +4,35 @@ import { cssIndex } from '../../styles/css/utils.styles.js'
 
 export const PortfolioDetailsContainer = styled.div`
   ${cssIndex.flexCenterCol}
+  ${cssIndex.keyFrames.fadein}
 
-  ul {
+  & > ul {
     img {
       max-width: 100px;
+    }
+  }
+
+  & > .description-long {
+    ul > li {
+      color: var(--white);
+      list-style: disc;
+    }
+
+    & > p > code {
+      background: #282a36;
+      color: #f1fa8c;
+      padding: 5px;
+    }
+
+    a:link,
+    a:visited {
+      color: var(--main);
+      transition: color 0.3s;
+
+      &:hover {
+        color: royalblue;
+        transition: color 0.3s;
+      }
     }
   }
 `
