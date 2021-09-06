b81af028c1d2962ad4fb18141fb05a062a82edc5
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-06 13:07:35 -0400

    added youtubeId to model

--- models/projectSchema.js
+++ models/projectSchema.js
@@ -25,20 +25,10 @@ const projectSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	/* later add an image
-	 Can upload image when creating new project
-	 image can be uploaded onto server as a static file
-	 to be referenced by client
-	 Images for:::
-	 gif
-	 cover
-	 other images
-
-	 Also create techStack icons and create component which renders them conditionally
-	 Whichever techStack strings appear in the array will determine which icons are rendered
-
-	 Make sure to make certain fields unique, such as slug since it's used for lookup
-	 */
+	youtubeId: {
+		type: String,
+		default: 'aaa',
+	},
 })
 
 const Project = mongoose.model('Project', projectSchema)
