e5f3089f944c36171f8a4cec275a977aba73dd0c
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-06 20:10:13 -0400

    frameBorder error

--- client/src/components/General/YtEmbed/YtEmbed.component.jsx
+++ client/src/components/General/YtEmbed/YtEmbed.component.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 import {} from './YtEmbed.styles'
 
-const YtEmbed = ({ linkId }) => {
+const YtEmbed = ({ linkId, className }) => {
 	return (
 		<>
 			<iframe
+				className={className}
 				width="560"
 				height="315"
 				src={`https://www.youtube.com/embed/${linkId}`}
 				title="YouTube video player"
-				frameborder="0"
+				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 			/>
