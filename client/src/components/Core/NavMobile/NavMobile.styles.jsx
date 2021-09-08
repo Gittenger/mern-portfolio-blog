0a789c086ca2d24a0b2063c8b6b7750b2d55aedc
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-08 02:06:50 -0400

    moved styles from navMobile to navList

--- client/src/components/Core/NavMobile/NavMobile.styles.jsx
+++ client/src/components/Core/NavMobile/NavMobile.styles.jsx
@@ -2,29 +2,17 @@ import styled from 'styled-components'
 
 import { cssIndex, device } from '../../../styles/css/utils.styles.js'
 
-const { tabletS, mobileMM } = device
+const { tabletS, tabletMM } = device
 
 export const NavMobileContainer = styled.nav`
 	position: fixed;
-	background: var(--black);
+	background: var(--blackDark);
 	bottom: 0;
 	left: 0;
 	z-index: 300;
 	width: 100%;
 	${cssIndex.flexCenter};
 	transition: transform 0.3s;
-
-	ul {
-		flex-direction: row;
-		width: 99%;
-		padding: 2.6rem 0;
-		font-size: 1rem;
-
-		& > li:not(:last-child) {
-			margin-bottom: 0;
-			margin-right: 1.6rem;
-		}
-	}
 
 	&.show {
 		transform: translateY(0);
@@ -34,21 +22,13 @@ export const NavMobileContainer = styled.nav`
 		transform: translateY(20rem);
 	}
 
+	${tabletMM} {
+		height: 6rem;
+	}
+
 	${tabletS} {
-		background: ${({ theme: { gradient } }) => gradient.navGradient};
 		width: 100vw;
-
-		ul {
-			flex-direction: column;
-			letter-spacing: 0.35em;
-			font-size: 1.5rem;
-			padding: 3rem 0;
-
-			& > li:not(:last-child) {
-				margin: 0;
-				margin-bottom: var(--m-SM);
-			}
-		}
+		height: unset;
 
 		&.show {
 			transform: translateY(0);
@@ -56,12 +36,6 @@ export const NavMobileContainer = styled.nav`
 
 		&.hide {
 			transform: translateY(70rem);
-		}
-	}
-
-	${mobileMM} {
-		ul {
-			padding: 3.5rem 0;
 		}
 	}
 `
