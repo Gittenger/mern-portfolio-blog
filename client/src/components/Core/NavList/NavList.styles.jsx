83752fcb83522a87e5399a7d1a84b4b6c86ba604
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-08 02:07:07 -0400

    mobile styles looking good

--- client/src/components/Core/NavList/NavList.styles.jsx
+++ client/src/components/Core/NavList/NavList.styles.jsx
@@ -4,7 +4,7 @@ import { cssIndex, device } from '../../../styles/css/utils.styles.js'
 
 import { navStyles } from '../../typography/typography.components.js'
 
-const { tabletS, tabletMM } = device
+const { tabletS, tabletMM, mobileMM } = device
 
 export const NavListContainer = styled.ul`
 	${navStyles}
@@ -14,91 +14,120 @@ export const NavListContainer = styled.ul`
 	li {
 		${cssIndex.flexCenter}
 		height: 100%;
-	}
 
-	.nav-link-box {
-		${cssIndex.flexCenter}
-		position: relative;
-		z-index: 0;
-		height: 100%;
-
-		// link with hover
-		a {
-			position: relative;
-			z-index: 10;
+		.nav-link-box {
 			${cssIndex.flexCenter}
-			padding: 0 1rem;
-			// transition: all 0.3s;
-
-			&:link,
-			&:visited {
-				color: var(--white);
-			}
-
-			&:hover + .nav-highlight {
-				opacity: 100%;
-			}
-
-			&.active-page {
-				filter: invert(35%);
-			}
-		}
-
-		// box under link for effect
-		& > .nav-highlight {
-			position: absolute;
+			position: relative;
 			z-index: 0;
-			opacity: 0;
-			background: var(--main);
-			top: 0;
-			left: -2px;
 			height: 100%;
-			width: 100%;
-			transform: skew(-3deg);
-			transition: all 0.3s;
 
-			&-1 {
-				filter: brightness(0.9);
+			// link with hover
+			a {
+				position: relative;
+				z-index: 10;
+				${cssIndex.flexCenter}
+				padding: 0 1rem;
+				// transition: all 0.3s;
+
+				&:link,
+				&:visited {
+					color: var(--white);
+				}
+
+				&:hover + .nav-highlight {
+					opacity: 100%;
+				}
+
+				// active-page effect
+				&.active-page {
+					filter: invert(35%);
+				}
 			}
-			&-2 {
-				filter: brightness(0.75);
-			}
-			&-3 {
-				filter: brightness(0.6);
-			}
-			&-4 {
-				filter: brightness(0.5);
-			}
-			&-5 {
-				filter: brightness(0.38);
-			}
-			&-6 {
-				filter: brightness(0.3);
-			}
-			&-7 {
-				filter: brightness(0.2);
+
+			// box under link for effect
+			& > .nav-highlight {
+				position: absolute;
+				z-index: 0;
+				opacity: 0;
+				background: var(--main);
+				top: 0;
+				left: -2px;
+				height: 100%;
+				width: 100%;
+				transform: skew(-3deg);
+				transition: all 0.3s;
+
+				&-1 {
+					filter: brightness(0.9);
+				}
+				&-2 {
+					filter: brightness(0.75);
+				}
+				&-3 {
+					filter: brightness(0.6);
+				}
+				&-4 {
+					filter: brightness(0.5);
+				}
+				&-5 {
+					filter: brightness(0.38);
+				}
+				&-6 {
+					filter: brightness(0.3);
+				}
+				&-7 {
+					filter: brightness(0.2);
+				}
 			}
 		}
 	}
 
+	// mid-size
 	${tabletMM} {
-		a:link,
-		a:visited {
-			color: var(--white);
+		width: 99%;
+		height: 100%;
+		font-size: 1.2rem;
 
-			&:hover {
-				color: var(--main);
-			}
+		& > li:not(:last-child) {
+			margin-bottom: 0;
+			margin-right: 1.6rem;
 		}
 	}
 
+	// small
 	${tabletS} {
-		a.active-page .nav-highlight {
-			display: none;
+		flex-direction: column;
+		letter-spacing: 0.35em;
+		font-size: 1.5rem;
+		width: 45%;
+		padding: 3rem 0;
+
+		& > li:not(:last-child) {
+			margin: 0;
 		}
 
 		li {
 			margin-right: 0;
+			width: 100%;
+
+			.nav-link-box {
+				width: 100%;
+				height: 100%;
+				padding: 2rem 0;
+
+				a {
+					&.active-page {
+						filter: none;
+					}
+				}
+
+				& > .nav-highlight {
+				}
+			}
 		}
+	}
+
+	${mobileMM} {
+		padding: 3.5rem 0;
 	}
 `
