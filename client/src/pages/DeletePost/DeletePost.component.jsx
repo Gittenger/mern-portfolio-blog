fbbd104b78609f6bed372bf302e0306043cdecfe
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-16 00:49:04 -0400

    deletePost working for single posts

--- client/src/pages/DeletePost/DeletePost.component.jsx
+++ client/src/pages/DeletePost/DeletePost.component.jsx
@@ -1,9 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { DeletePostContentContainer } from './DeletePost.styles'
+import { DeletePostContentContainer, Row } from './DeletePost.styles'
+import CIndex from '../../components/components.index'
 
 const DeletePost = () => {
-	return <DeletePostContentContainer></DeletePostContentContainer>
+	const [values, setValues] = useState({
+		posts: [],
+	})
+	const url = `${process.env.REACT_APP_API}/posts`
+
+	useEffect(() => {
+		let unmounted = false
+		if (unmounted) return
+
+		fetch(url, {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			method: 'GET',
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (!unmounted) {
+					setValues({ ...values, posts: res.data })
+				}
+			})
+			.catch((err) => console.error(err))
+
+		return () => {
+			unmounted = true
+		}
+	}, [])
+
+	const handleDelete = (e) => {
+		const deleteUrl = `${process.env.REACT_APP_API}/posts/${e.target.dataset.id}`
+		fetch(deleteUrl, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		})
+			.then((res) => {
+				console.log(res)
+				window.location.reload()
+			})
+			.catch((err) => console.error(err))
+	}
+
+	const {
+		TComp: { PSmall },
+	} = CIndex
+
+	return (
+		<DeletePostContentContainer>
+			<ul>
+				{values.posts.map((post, i) => (
+					<li key={i}>
+						<Row>
+							<PSmall>Post name: {post.title}</PSmall>
+							<button data-id={post._id} onClick={handleDelete}>
+								Delete
+							</button>
+						</Row>
+					</li>
+				))}
+			</ul>
+		</DeletePostContentContainer>
+	)
 }
 
 export default DeletePost
--- client/src/pages/DeletePost/DeletePost.styles.jsx
+++ client/src/pages/DeletePost/DeletePost.styles.jsx
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
 
-export const DeletePostContentContainer = styled.div``
+import { cssIndex } from '../../styles/css/utils.styles.js'
+
+export const DeletePostContentContainer = styled.div`
+	ul {
+		& > * {
+			margin-bottom: 2rem;
+		}
+	}
+`
+
+export const Row = styled.div`
+	${cssIndex.flexCenter}
+
+	p {
+		margin-bottom: 0;
+	}
+
+	button {
+		margin-left: 0.7rem;
+		cursor: pointer;
+	}
+`
