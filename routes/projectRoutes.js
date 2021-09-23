eed689a781833a40858a96f57732e7837537f2fe
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-23 19:22:31 -0400

    update and delete routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -3,10 +3,19 @@ const projectController = require('../controllers/projectController')
 
 const router = express.Router()
 
-const { getAll, getOne, createProject } = projectController
+const {
+	getAll,
+	getOne,
+	createProject,
+	deleteProject,
+	updateProject,
+} = projectController
 
 router.get('/', getAll)
 router.get('/:slug', getOne)
+
 router.post('/', createProject)
+router.post('/:id', updateProject)
+router.delete('/:id', deleteProject)
 
 module.exports = router
