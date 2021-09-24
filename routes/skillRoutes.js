99078f44a76e242970faa90fddcab757e15678ac
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-24 01:25:23 -0400

    getOne, update

--- routes/skillRoutes.js
+++ routes/skillRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const skillsController = require('../controllers/skillsController')
 
-const { createCard, getAll } = skillsController
+const { createCard, getAll, getOne, updateCard } = skillsController
 
 const router = express.Router()
 
 router.get('/', getAll)
+router.get('/:slug', getOne)
 router.post('/', createCard)
+router.post('/:id', updateCard)
 
 module.exports = router
