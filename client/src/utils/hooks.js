4e2ef7e165b707eaf88fff34f1308dc6ffd056de
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-26 13:07:13 -0400

    hook for getting api data

--- client/src/utils/hooks.js
+++ client/src/utils/hooks.js
@@ -55,3 +55,67 @@ export const useShouldUpdateCache = (url) => {
 
 	return shouldFetchNewApi
 }
+
+export const useApiData = (url) => {
+	const [apiData, setApiData] = useState({
+		data: {
+			key: {},
+		},
+		timeStamp: '',
+	})
+	const [dataProcessed, setDataProcessed] = useState(false)
+
+	const { urls, setUrlsContext } = useContext(UrlsContext)
+
+	const shouldFetchNewApi = useShouldUpdateCache(url)
+	const cachedItem = JSON.parse(localStorage.getItem(url))
+
+	useEffect(() => {
+		let unmounted = false
+		if (unmounted) return
+
+		if (cachedItem && shouldFetchNewApi === false) {
+			console.log('from local')
+			if (!unmounted) {
+				setApiData(cachedItem)
+				setDataProcessed(true)
+			}
+			setUrlsContext(url, cachedItem)
+		} else if (urls[url] && shouldFetchNewApi === false) {
+			console.log('from context')
+			if (!unmounted) {
+				setApiData(urls[url])
+				setDataProcessed(true)
+			}
+		} else {
+			fetch(url, {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			})
+				.then((res) => res.json())
+				.then((res) => {
+					const fromData = {}
+					res.data.forEach((item) => {
+						fromData[item.slug] = item
+					})
+					const dataObject = { timeStamp: new Date(), data: fromData }
+					localStorage.setItem(url, JSON.stringify(dataObject))
+					setUrlsContext(url, dataObject)
+					if (!unmounted) {
+						setApiData(dataObject)
+						setDataProcessed(true)
+					}
+				})
+				.catch((err) => console.error(err))
+		}
+
+		return () => {
+			unmounted = true
+		}
+	}, [shouldFetchNewApi])
+
+	return [apiData, dataProcessed]
+}
