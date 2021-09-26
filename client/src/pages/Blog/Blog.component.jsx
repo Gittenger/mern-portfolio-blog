a9e26336b5ae55429c122313c3c476ade45b449b
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-09-26 13:07:27 -0400

    implementing apiData hook

--- client/src/pages/Blog/Blog.component.jsx
+++ client/src/pages/Blog/Blog.component.jsx
@@ -1,80 +1,23 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { useShouldUpdateCache } from '../../utils/hooks.js'
+import React from 'react'
+import { useApiData } from '../../utils/hooks.js'
 import { Link } from 'react-router-dom'
 
 import { BlogPageContentContainer } from './Blog.styles'
 
-import UrlsContext from '../../contexts/UrlsContext.js'
-
 const Blog = () => {
-	const [values, setValues] = useState({
-		data: {
-			key: {
-				slug: '',
-				title: '',
-			},
-		},
-		timeStamp: '',
-	})
-	const { urls, setUrlsContext } = useContext(UrlsContext)
-
 	const url = `${process.env.REACT_APP_API}/posts`
-	const shouldFetchNewApi = useShouldUpdateCache(url)
+	const [apiData, dataProcessed] = useApiData(url)
 
-	const cachedItem = JSON.parse(localStorage.getItem(url))
-
-	useEffect(() => {
-		let unmounted = false
-		if (unmounted) return
-
-		if (cachedItem && shouldFetchNewApi === false) {
-			console.log('from local')
-			if (!unmounted) {
-				setValues(cachedItem)
-			}
-			setUrlsContext(url, cachedItem)
-		} else if (urls[url] && shouldFetchNewApi === false) {
-			console.log('from context')
-			if (!unmounted) {
-				setValues(urls[url])
-			}
-		} else {
-			console.log('from api')
-			fetch(url, {
-				method: 'GET',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-			})
-				.then((res) => res.json())
-				.then((res) => {
-					const fromData = {}
-					res.data.forEach((post) => {
-						fromData[post.slug] = post
-					})
-					const dataObject = { timeStamp: new Date(), data: fromData }
-					localStorage.setItem(url, JSON.stringify(dataObject))
-					setUrlsContext(url, dataObject)
-					if (!unmounted) {
-						setValues(dataObject)
-					}
-				})
-				.catch((err) => console.error(err))
-		}
-
-		return () => {
-			unmounted = true
-		}
-	}, [shouldFetchNewApi])
+	const { data } = apiData
 
 	return (
 		<BlogPageContentContainer>
-			{Object.keys(values.data).map((post, i) => (
-				<Link key={i} to={`/blog/posts/${values.data[post].slug}`}>
-					{values.data[post].title}
-				</Link>
-			))}
+			{dataProcessed &&
+				Object.keys(data).map((post, i) => (
+					<Link key={i} to={`/blog/posts/${data[post].slug}`}>
+						{data[post].title}
+					</Link>
+				))}
 		</BlogPageContentContainer>
 	)
 }
--- client/src/pages/Portfolio/Portfolio.component.jsx
+++ client/src/pages/Portfolio/Portfolio.component.jsx
@@ -1,87 +1,31 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { useShouldUpdateCache } from '../../utils/hooks.js'
+import React from 'react'
+import { useApiData } from '../../utils/hooks.js'
 
 import CIndex from '../../components/components.index.js'
 import { PortfolioPageContainer, PortfolioCardList } from './Portfolio.styles'
 
-import UrlsContext from '../../contexts/UrlsContext.js'
-
 const Portfolio = () => {
 	const { ProjectCard } = CIndex
-	const [values, setValues] = useState({
-		data: {
-			key: {
-				name: '',
-				description: '',
-				slug: '',
-			},
-		},
-		timeStamp: '',
-	})
-	const { urls, setUrlsContext } = useContext(UrlsContext)
 
 	const url = `${process.env.REACT_APP_API}/projects`
-	const shouldFetchNewApi = useShouldUpdateCache(url)
+	const [apiData, dataProcessed] = useApiData(url)
 
-	const cachedItem = JSON.parse(localStorage.getItem(url))
-
-	useEffect(() => {
-		let unmounted = false
-		if (unmounted) return
-
-		if (cachedItem && shouldFetchNewApi === false) {
-			console.log('from local')
-			if (!unmounted) {
-				setValues(cachedItem)
-			}
-			setUrlsContext(url, cachedItem)
-		} else if (urls[url] && shouldFetchNewApi === false) {
-			console.log('from context')
-			if (!unmounted) {
-				setValues(urls[url])
-			}
-		} else {
-			fetch(url, {
-				method: 'GET',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-			})
-				.then((res) => res.json())
-				.then((res) => {
-					const fromData = {}
-					res.data.forEach((project) => {
-						fromData[project.slug] = project
-					})
-					const dataObject = { timeStamp: new Date(), data: fromData }
-					localStorage.setItem(url, JSON.stringify(dataObject))
-					setUrlsContext(url, dataObject)
-					if (!unmounted) {
-						setValues(dataObject)
-					}
-				})
-				.catch((err) => console.error(err))
-		}
-
-		return () => {
-			unmounted = true
-		}
-	}, [shouldFetchNewApi])
+	const { data } = apiData
 
 	return (
 		<PortfolioPageContainer>
 			<PortfolioCardList>
-				{Object.keys(values.data).map((project, i) => (
-					<li key={i}>
-						<ProjectCard
-							title={values.data[project].name}
-							description={values.data[project].description}
-							anchor={`portfolio/${values.data[project].slug}`}
-							coverImg={values.data[project].slug}
-						/>
-					</li>
-				))}
+				{dataProcessed &&
+					Object.keys(data).map((project, i) => (
+						<li key={i}>
+							<ProjectCard
+								title={data[project].name}
+								description={data[project].description}
+								anchor={`portfolio/${data[project].slug}`}
+								coverImg={data[project].slug}
+							/>
+						</li>
+					))}
 			</PortfolioCardList>
 		</PortfolioPageContainer>
 	)
