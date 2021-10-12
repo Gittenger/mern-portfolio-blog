ca0bd2c0f6531e9f7540d0d9168e1dbb2d4ef743
Gittenger <36045366+Gittenger@users.noreply.github.com>
2021-10-12 12:20:32 -0400

    spinner component

--- client/src/components/components.index.js
+++ client/src/components/components.index.js
@@ -1,31 +1,31 @@
 import AdminMenu from './Admin/AdminMenu/AdminMenu.component.jsx'
 import AdminRoute from './Admin/AdminRoute/AdminRoute.component.jsx'
 import ChangePassword from './Admin/ChangePassword/ChangePassword.component.jsx'
-import Content from './Core/Content/Content.component.jsx'
 import ForgotPassword from './Admin/ForgotPassword/ForgotPassword.component.jsx'
 import ResetPassword from './Admin/ResetPassword/ResetPassword.component.jsx'
-import ProjectCard from './General/ProjectCard/ProjectCard.component.jsx'
+import Content from './Core/Content/Content.component.jsx'
 import Footer from './Core/Footer/Footer.component.jsx'
 import Layout from './Core/Layout/Layout.component.jsx'
 import NavDesk from './Core/NavDesk/NavDesk.component.jsx'
 import NavList from './Core/NavList/NavList.component.jsx'
 import NavMobile from './Core/NavMobile/NavMobile.component.jsx'
 import ToggleNavButton from './Core/ToggleNavButton/ToggleNavButton.component.jsx'
-import SkillCard from './General/SkillCard/SkillCard.component.jsx'
 import Checkbox from './General/Checkbox/Checkbox.component.jsx'
 import ContactForm from './General/ContactForm/ContactForm.component.jsx'
-import SkillImage from './General/SkillImage/SkillImage.component.jsx'
+import ProjectCard from './General/ProjectCard/ProjectCard.component.jsx'
+import SkillCard from './General/SkillCard/SkillCard.component.jsx'
 import SkillIconBox from './General/SkillIconBox/SkillIconBox.component.jsx'
+import SkillImage from './General/SkillImage/SkillImage.component.jsx'
+import Spinner from './General/Spinner/Spinner.component.jsx'
 import YtEmbed from './General/YtEmbed/YtEmbed.component.jsx'
 import TComp from './typography/typography.components.js'
 export const CIndex = {
 	AdminMenu,
 	AdminRoute,
 	ChangePassword,
-	Content,
 	ForgotPassword,
 	ResetPassword,
-	ProjectCard,
+	Content,
 	Footer,
 	Layout,
 	NavDesk,
@@ -34,9 +34,11 @@ export const CIndex = {
 	ToggleNavButton,
 	Checkbox,
 	ContactForm,
+	ProjectCard,
 	SkillCard,
-	SkillImage,
 	SkillIconBox,
+	SkillImage,
+	Spinner,
 	YtEmbed,
 	TComp,
 }
