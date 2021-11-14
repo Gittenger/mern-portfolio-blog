b453d853e7aa4759bf74d1898d848681d31b933b
gittenger <john.pittenger86@gmail.com>
2021-11-13 20:52:45 -0500

    check for update create new if none

--- controllers/checkForUpdateController.js
+++ controllers/checkForUpdateController.js
@@ -3,10 +3,16 @@ const catchAsync = require('../utils/catchAsync')
 
 exports.checkForUpdate = catchAsync(async (req, res) => {
 	const lastUpdated = await ServerUpdated.find()
-
-	res.status(200).json({
-		lastUpdated: lastUpdated[0].value,
-	})
+	if (lastUpdated[0]) {
+		res.status(200).json({
+			lastUpdated: lastUpdated[0].value,
+		})
+	} else {
+		valueObj = await ServerUpdated.create({ value: Date.now() })
+		res.status(200).json({
+			lastUpdated: valueObj.value,
+		})
+	}
 })
 
 exports.createInitValue = catchAsync(async (req, res) => {
