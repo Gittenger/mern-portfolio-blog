7b8d1911fd983759e4379c77f2c50b7269d687a8
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-15 12:12:16 -0400

    resume page temp OK

--- client/src/pages/Resume/Resume.component.jsx
+++ client/src/pages/Resume/Resume.component.jsx
@@ -3,39 +3,45 @@ import React from 'react'
 import { ResumePageContentContainer } from './Resume.styles'
 
 const Resume = () => {
-	const downloadUrl = `${process.env.REACT_APP_PUBLIC}/pdf/resume.pdf`
+  const downloadUrl = `${process.env.REACT_APP_PUBLIC}/pdf/resume.pdf`
 
-	const download = (url, name) => {
-		fetch(url)
-			.then((res) => res.blob())
-			.then((blob) => {
-				const blobURL = URL.createObjectURL(blob)
-				const a = document.createElement('a')
-				a.href = blobURL
-				a.style = 'display: none'
+  const download = (url, name) => {
+    fetch(url)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const blobURL = URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = blobURL
+        a.style = 'display: none'
 
-				if (name && name.length) a.download = name
-				document.body.appendChild(a)
-				a.click()
-			})
-	}
+        if (name && name.length) a.download = name
+        document.body.appendChild(a)
+        a.click()
+      })
+  }
 
-	const driveUrl =
-		'https://drive.google.com/file/d/1fqA2Q1UlK8iUHy-x-HAM-t-8TVFGBTbq/view?usp=sharing'
+  const driveUrl =
+    'https://drive.google.com/file/d/1fqA2Q1UlK8iUHy-x-HAM-t-8TVFGBTbq/view?usp=sharing'
 
-	return (
-		<ResumePageContentContainer>
-			<h1>Resume</h1>
-			<p>Link to download:</p>
-			<button onClick={() => download(downloadUrl, 'my-resume.pdf')}>
-				Download resume
-			</button>
-			<p>Link to view on Google Drive:</p>
-			<a href={driveUrl} target="_blank">
-				View on Drive
-			</a>
-		</ResumePageContentContainer>
-	)
+  return (
+    <ResumePageContentContainer>
+      <h1>Resume...</h1>
+      <div>
+        <div>
+          <p>Link to download:</p>
+          <button onClick={() => download(downloadUrl, 'my-resume.pdf')}>
+            Download resume
+          </button>
+        </div>
+        <div>
+          <p>Link to view on Google Drive:</p>
+          <a href={driveUrl} target="_blank">
+            View on Drive
+          </a>
+        </div>
+      </div>
+    </ResumePageContentContainer>
+  )
 }
 
 export default Resume
--- client/src/pages/Resume/Resume.styles.jsx
+++ client/src/pages/Resume/Resume.styles.jsx
@@ -1,17 +1,56 @@
 import styled from 'styled-components'
 
-import { cssIndex } from '../../styles/css/utils.styles.js'
+import { cssIndex, device } from '../../styles/css/utils.styles.js'
+
+const { tabletM } = device
 
 export const ResumePageContentContainer = styled.div`
   ${cssIndex.flexCenterCol}
   color: white;
 
-  button {
-    background: none;
-    border: none;
-    color: var(--main);
-    text-decoration: underline;
-    font-size: 1.2em;
-    cursor: pointer;
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  & > div {
+    ${cssIndex.flexCenter}
+    align-items: flex-start;
+
+    ${tabletM} {
+      ${cssIndex.flexCenterCol}
+    }
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: center;
+      width: 300px;
+
+      ${tabletM} {
+        margin: 1rem 0;
+        width: 220px;
+      }
+
+      & > * {
+        text-align: center;
+      }
+    }
+
+    button,
+    a,
+    a:visited,
+    a:link {
+      background: none;
+      border: none;
+      color: var(--main);
+      text-decoration: underline;
+      font-size: 1.2em;
+      cursor: pointer;
+
+      &:hover {
+        color: royalblue;
+      }
+    }
   }
 `
