0ff23e2bc68e4ede4d7da464f11c730c93cbdf54
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-15 13:09:21 -0400

    contact emails working OK

--- utils/email.js
+++ utils/email.js
@@ -2,127 +2,125 @@ const nodemailer = require('nodemailer')
 const nodemailerSendgrid = require('nodemailer-sendgrid')
 const htmlToText = require('html-to-text')
 const pug = require('pug')
+const path = require('path')
 
 class EmailUser {
-	constructor(user, url) {
-		this.to = user.email
-		this.name = user.name
-		this.url = url
-		this.from = `${process.env.SITE_TITLE} <${process.env.EMAIL_FROM}>`
-	}
+  constructor(user, url) {
+    this.to = user.email
+    this.name = user.name
+    this.url = url
+    this.from = `${process.env.SITE_TITLE} <${process.env.EMAIL_FROM}>`
+  }
 
-	newTransport() {
-		if (process.env.NODE_ENV == 'production') {
-			// use sendgrid
-			return nodemailer.createTransport(
-				nodemailerSendgrid({ apiKey: process.env.SENDGRID_API_KEY })
-			)
-		} else {
-			return nodemailer.createTransport({
-				host: process.env.EMAIL_HOST,
-				port: process.env.EMAIL_PORT,
-				auth: {
-					user: process.env.EMAIL_USERNAME,
-					pass: process.env.EMAIL_PASSWORD,
-				},
-			})
-		}
-	}
+  newTransport() {
+    if (process.env.NODE_ENV == 'production') {
+      // use sendgrid
+      return nodemailer.createTransport(
+        nodemailerSendgrid({ apiKey: process.env.SENDGRID_API_KEY })
+      )
+    } else {
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      })
+    }
+  }
 
-	async send(template, subject) {
-		const html = pug.renderFile(
-			`${__dirname}/../mailTemplates/user/${template}.pug`,
-			{
-				url: this.url,
-				name: this.name,
-				subject,
-			}
-		)
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../mailTemplates/user/${template}.pug`,
+      {
+        url: this.url,
+        name: this.name,
+        subject,
+      }
+    )
 
-		const mailOptions = {
-			from: this.from,
-			to: this.to,
-			subject,
-			html,
-			text: htmlToText.htmlToText(html),
-		}
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    }
 
-		await this.newTransport().sendMail(mailOptions)
-	}
+    await this.newTransport().sendMail(mailOptions)
+  }
 
-	async sendWelcome() {
-		await this.send('welcome', `Welcome to ${process.env.SITE_TITLE}`)
-	}
+  async sendWelcome() {
+    await this.send('welcome', `Welcome to ${process.env.SITE_TITLE}`)
+  }
 
-	async sendPasswordReset() {
-		await this.send(
-			'resetPassword',
-			'Your password reset token (expires in 10 minutes)'
-		)
-	}
+  async sendPasswordReset() {
+    await this.send(
+      'resetPassword',
+      'Your password reset token (expires in 10 minutes)'
+    )
+  }
 }
 
 class EmailContact {
-	constructor(formInfo) {
-		this.name = formInfo.name
-		this.email = formInfo.email
-		this.desc = formInfo.desc
-		this.phone = formInfo.phone
-		this.from = `${process.env.SITE_TITLE} <${process.env.EMAIL_FROM}>`
-	}
+  constructor(formInfo) {
+    this.name = formInfo.name
+    this.email = formInfo.email
+    this.desc = formInfo.desc
+    this.phone = formInfo.phone
+    this.from = `${process.env.SITE_TITLE} <${process.env.EMAIL_FROM}>`
+  }
 
-	newTransport() {
-		if (process.env.NODE_ENV === 'production') {
-			// Sendgrid
-			return nodemailer.createTransport(
-				nodemailerSendgrid({ apiKey: process.env.SENDGRID_API_KEY })
-			)
-		}
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return nodemailer.createTransport(
+        nodemailerSendgrid({ apiKey: process.env.SENDGRID_API_KEY })
+      )
+    }
 
-		return nodemailer.createTransport({
-			host: process.env.EMAIL_HOST,
-			port: process.env.EMAIL_PORT,
-			auth: {
-				user: process.env.EMAIL_USERNAME,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		})
-	}
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+  }
 
-	async send(template, subject, options = { copy: false }) {
-		const html = pug.renderFile(
-			`${__dirname}/../mailTemplates/contact/${template}.pug`,
-			{
-				name: this.name,
-				email: this.email,
-				desc: this.desc,
-				phone: this.phone,
-			}
-		)
+  async send(template, subject, options = { copy: false }) {
+    const html = pug.renderFile(
+      path.resolve(__dirname, '../mailTemplates/contact', `${template}.pug`),
+      {
+        name: this.name,
+        email: this.email,
+        desc: this.desc,
+        phone: this.phone,
+      }
+    )
 
-		const mailOptions = {
-			from: this.from,
-			to: options.copy ? this.email : process.env.MASTER_EMAIL,
-			subject,
-			html,
-			text: htmlToText.htmlToText(html),
-		}
+    const mailOptions = {
+      from: this.from,
+      to: options.copy ? this.email : process.env.MASTER_EMAIL,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    }
 
-		return await this.newTransport().sendMail(mailOptions)
-	}
+    return await this.newTransport().sendMail(mailOptions)
+  }
 
-	async sendGreeting() {
-		return await this.send(
-			'main',
-			`New email from your ${process.env.SITE_TITLE} website`
-		)
-	}
+  async sendGreeting() {
+    return await this.send('main', `New email from your portfolio site`)
+  }
 
-	async sendCopy() {
-		return await this.send('copy', `Your email to ${process.env.SITE_TITLE}`, {
-			copy: true,
-		})
-	}
+  async sendCopy() {
+    return await this.send('copy', `Your email to ${process.env.SITE_TITLE}`, {
+      copy: true,
+    })
+  }
 }
 
 module.exports.EmailUser = EmailUser
