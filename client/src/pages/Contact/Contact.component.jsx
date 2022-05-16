885fdeafcdd9cc8bf9ad08bc285697b66ec72226
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-16 02:23:32 -0400

    contact temporary page

--- client/src/pages/Contact/Contact.component.jsx
+++ client/src/pages/Contact/Contact.component.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect } from 'react'
 
 import CIndex from '../../components/components.index.js'
-import { ContactPageContainer } from './Contact.styles.jsx'
+import { ContactPageContainer, ContactTempBox } from './Contact.styles.jsx'
 
 const Contact = () => {
-	const { ContactForm } = CIndex
+  const { ContactForm, TComp } = CIndex
+  const { PSmall } = TComp
 
-	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
-	return (
-		<ContactPageContainer>
-			<ContactForm />
-		</ContactPageContainer>
-	)
+  return (
+    <ContactPageContainer>
+      {/* <ContactForm /> */}
+      <ContactTempBox>
+        <PSmall>
+          I'm currently fixing my automated email system by migrating to a new
+          provider, <a href="https://www.sendinblue.com/">SendInBlue</a>.
+        </PSmall>
+        <PSmall>
+          In the meantime, feel free to reach out and email me at:
+        </PSmall>
+        <a href="mailto:john@johnpittenger.dev">john@johnpittenger.dev</a>
+      </ContactTempBox>
+    </ContactPageContainer>
+  )
 }
 
 export default Contact
--- client/src/pages/Contact/Contact.styles.jsx
+++ client/src/pages/Contact/Contact.styles.jsx
@@ -6,21 +6,39 @@ import { cssIndex, device } from '../../styles/css/utils.styles.js'
 const { tabletLand, mobileMM, mobileM, mobileS } = device
 
 export const ContactPageContainer = styled.div`
-	${cssIndex.flexCenter}
-	flex-direction: column;
-	width: 100%;
-	padding: 0 0 15rem;
-	${cssIndex.keyFrames.fadein}
+  ${cssIndex.flexCenter}
+  flex-direction: column;
+  width: 100%;
+  padding: 0 0 15rem;
+  ${cssIndex.keyFrames.fadein}
 
-	${tabletLand} {
-		padding: 2rem 0 7rem;
-	}
+  ${tabletLand} {
+    padding: 2rem 0 7rem;
+  }
 
-	${mobileMM} {
-		padding: 1rem 0 5rem;
-	}
+  ${mobileMM} {
+    padding: 1rem 0 5rem;
+  }
 
-	${mobileS} {
-		padding: 1rem 0 3rem;
-	}
+  ${mobileS} {
+    padding: 1rem 0 3rem;
+  }
+`
+
+export const ContactTempBox = styled.div`
+  ${cssIndex.flexCenterCol}
+
+  a,
+  a:visited,
+  a:link {
+    color: var(--main);
+
+    &:hover {
+      color: royalblue;
+    }
+  }
+
+  p {
+    --scale: 0.55;
+  }
 `
