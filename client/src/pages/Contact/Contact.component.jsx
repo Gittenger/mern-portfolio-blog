dc86aae17633d89b261d6be6345acf0412ade937
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-24 02:46:33 -0400

    remove contact tmp

--- client/src/pages/Contact/Contact.component.jsx
+++ client/src/pages/Contact/Contact.component.jsx
@@ -14,16 +14,6 @@ const Contact = () => {
   return (
     <ContactPageContainer>
       <ContactForm />
-      {/* <ContactTempBox>
-        <PSmall>
-          I'm currently fixing my automated email system by migrating to a new
-          provider, <a href="https://www.sendinblue.com/">SendInBlue</a>.
-        </PSmall>
-        <PSmall>
-          In the meantime, feel free to reach out and email me at:
-        </PSmall>
-        <a href="mailto:john@johnpittenger.dev">john@johnpittenger.dev</a>
-      </ContactTempBox> */}
     </ContactPageContainer>
   )
 }
