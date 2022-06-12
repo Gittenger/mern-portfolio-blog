46c56a44f8ece3e0df2509899e5d5095a52a40f8
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-06-12 18:58:02 -0400

    Portfolio ordering working

--- client/src/pages/Portfolio/Portfolio.component.jsx
+++ client/src/pages/Portfolio/Portfolio.component.jsx
@@ -1,37 +1,67 @@
-import React from 'react'
-import { useApiData } from '../../utils/hooks.js'
+import React, { useState, useEffect } from 'react'
+import { useApiData, useSortedData } from '../../utils/hooks.js'
 
 import CIndex from '../../components/components.index.js'
 import { PortfolioPageContainer, PortfolioCardList } from './Portfolio.styles'
 
 const Portfolio = () => {
-	const { ProjectCard, Spinner } = CIndex
+  const { ProjectCard, Spinner } = CIndex
+  const [sortedData, setSortedData] = useState([])
 
-	const url = `${process.env.REACT_APP_API}/projects`
-	const [apiData, dataProcessed] = useApiData(url)
+  const url = `${process.env.REACT_APP_API}/projects`
+  const [apiData, dataProcessed] = useApiData(url)
 
-	const { data } = apiData
+  const { data } = apiData
 
-	return (
-		<PortfolioPageContainer>
-			<PortfolioCardList>
-				{dataProcessed ? (
-					Object.keys(data).map((project, i) => (
-						<li key={i}>
-							<ProjectCard
-								title={data[project].name}
-								description={data[project].description}
-								anchor={`portfolio/${data[project].slug}`}
-								coverImg={data[project].slug}
-							/>
-						</li>
-					))
-				) : (
-					<Spinner />
-				)}
-			</PortfolioCardList>
-		</PortfolioPageContainer>
-	)
+  useEffect(() => {
+    const order = [
+      'fantastic-flames',
+      'pokedex',
+      'frontend-mentor-projects',
+      'mern-portfolio-site',
+      'mern-template-demo',
+      'art-shoppe',
+      'natours',
+      'mock-portfolio',
+      'javascript-30',
+    ]
+
+    const dataKeys = Object.keys(data)
+
+    const itemPositions = {}
+    for (const [index, name] of order.entries()) {
+      itemPositions[name] = index
+    }
+
+    const sorted = dataKeys
+      .sort((a, b) => itemPositions[a] - itemPositions[b])
+      .map((name) => {
+        return { ...data[name] }
+      })
+
+    setSortedData(sorted)
+  }, [data])
+
+  return (
+    <PortfolioPageContainer>
+      <PortfolioCardList>
+        {dataProcessed ? (
+          sortedData.map((project, i) => (
+            <li key={i}>
+              <ProjectCard
+                title={project.name}
+                description={project.description}
+                anchor={`portfolio/${project.slug}`}
+                coverImg={project.slug}
+              />
+            </li>
+          ))
+        ) : (
+          <Spinner />
+        )}
+      </PortfolioCardList>
+    </PortfolioPageContainer>
+  )
 }
 
 export default Portfolio
