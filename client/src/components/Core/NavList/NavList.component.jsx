535cb2530921514cb2977889ee15483fb9a40e8c
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-06-13 19:45:50 -0400

    navbar MQ

--- client/src/components/Core/NavList/NavList.component.jsx
+++ client/src/components/Core/NavList/NavList.component.jsx
@@ -1,59 +1,59 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { NavListContainer } from './NavList.styles'
+import { NavLinkBox, NavListContainer } from './NavList.styles'
 
 const NavList = () => (
-	<NavListContainer>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="home" to="/">
-					Home
-				</Link>
-				<div className="nav-highlight nav-highlight-1"></div>
-			</div>
-		</li>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="contact" to="/contact">
-					Contact
-				</Link>
-				<div className="nav-highlight nav-highlight-2"></div>
-			</div>
-		</li>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="portfolio" to="/portfolio">
-					Portfolio
-				</Link>
-				<div className="nav-highlight nav-highlight-3"></div>
-			</div>
-		</li>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="skills" to="/skills">
-					Skills
-				</Link>
-				<div className="nav-highlight nav-highlight-4"></div>
-			</div>
-		</li>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="blog" to="/blog">
-					Blog
-				</Link>
-				<div className="nav-highlight nav-highlight-5"></div>
-			</div>
-		</li>
-		<li>
-			<div className="nav-link-box">
-				<Link data-page="resume" to="/resume">
-					Resume
-				</Link>
-				<div className="nav-highlight nav-highlight-6"></div>
-			</div>
-		</li>
-	</NavListContainer>
+  <NavListContainer>
+    <li>
+      <NavLinkBox>
+        <Link data-page="home" to="/">
+          Home
+        </Link>
+        <div className="nav-highlight nav-highlight-1"></div>
+      </NavLinkBox>
+    </li>
+    <li>
+      <NavLinkBox>
+        <Link data-page="contact" to="/contact">
+          Contact
+        </Link>
+        <div className="nav-highlight nav-highlight-2"></div>
+      </NavLinkBox>
+    </li>
+    <li>
+      <NavLinkBox>
+        <Link data-page="portfolio" to="/portfolio">
+          Portfolio
+        </Link>
+        <div className="nav-highlight nav-highlight-3"></div>
+      </NavLinkBox>
+    </li>
+    <li>
+      <NavLinkBox>
+        <Link data-page="skills" to="/skills">
+          Skills
+        </Link>
+        <div className="nav-highlight nav-highlight-4"></div>
+      </NavLinkBox>
+    </li>
+    <li>
+      <NavLinkBox>
+        <Link data-page="blog" to="/blog">
+          Blog
+        </Link>
+        <div className="nav-highlight nav-highlight-5"></div>
+      </NavLinkBox>
+    </li>
+    <li>
+      <NavLinkBox>
+        <Link data-page="resume" to="/resume">
+          Resume
+        </Link>
+        <div className="nav-highlight nav-highlight-6"></div>
+      </NavLinkBox>
+    </li>
+  </NavListContainer>
 )
 
 export default NavList
--- client/src/components/Core/NavList/NavList.styles.jsx
+++ client/src/components/Core/NavList/NavList.styles.jsx
@@ -4,133 +4,124 @@ import { cssIndex, device } from '../../../styles/css/utils.styles.js'
 
 import { navStyles } from '../../typography/typography.components.js'
 
-const { tabletS, tabletMM, mobileMM } = device
+const { tabletS, tabletMM, mobileMM, tabletL } = device
 
 export const NavListContainer = styled.ul`
-	${navStyles}
-	${cssIndex.flexCenter}
+  ${navStyles}
+  ${cssIndex.flexCenter}
 	height: 100%;
+  width: 100%;
 
-	li {
-		${cssIndex.flexCenter}
-		height: 100%;
+  li {
+    ${cssIndex.flexCenter}
+    height: 100%;
+  }
 
-		.nav-link-box {
-			${cssIndex.flexCenter}
-			position: relative;
-			z-index: 0;
-			height: 100%;
+  ${tabletL} {
+    font-size: 1.5rem;
+  }
 
-			// link with hover
-			a {
-				position: relative;
-				z-index: 10;
-				${cssIndex.flexCenter}
-				padding: 0 1rem;
-				height: 100%;
+  ${tabletMM} {
+    font-size: 1.2rem;
+  }
 
-				// inverse skew on text
-				transform: var(--inverseSkewNav);
+  // small
+  ${tabletS} {
+    flex-direction: column;
+    letter-spacing: 0.35em;
+    font-size: 1.4rem;
+    padding: 3rem 0;
+  }
 
-				&:link,
-				&:visited {
-					color: var(--white);
-				}
+  ${mobileMM} {
+    padding: 3.5rem 0;
+  }
+`
 
-				&:hover + .nav-highlight {
-					opacity: 100%;
-				}
+export const NavLinkBox = styled.div`
+  ${cssIndex.flexCenter}
+  position: relative;
+  z-index: 0;
+  height: 100%;
 
-				// active-page effect
-				&.active-page {
-					filter: invert(35%);
-				}
-			}
+  // link with hover
+  a {
+    position: relative;
+    z-index: 10;
+    ${cssIndex.flexCenter}
+    padding: 0 1rem;
+    height: 100%;
 
-			// box under link for effect
-			& > .nav-highlight {
-				position: absolute;
-				z-index: 0;
-				opacity: 0;
-				background: var(--main);
-				top: 0;
-				left: -2px;
-				height: 100%;
-				width: 100%;
-				transform: skew(-3deg);
-				transition: all 0.3s;
+    &:link,
+    &:visited {
+      color: var(--white);
+    }
 
-				&-1 {
-					filter: brightness(0.9);
-				}
-				&-2 {
-					filter: brightness(0.82);
-				}
-				&-3 {
-					filter: brightness(0.74);
-				}
-				&-4 {
-					filter: brightness(0.65);
-				}
-				&-5 {
-					filter: brightness(0.57);
-				}
-				&-6 {
-					filter: brightness(0.49);
-				}
-				&-7 {
-					filter: brightness(0.41);
-				}
-			}
-		}
-	}
+    ${tabletL} {
+      padding: 0 0.5rem;
+    }
 
-	// mid-size
-	${tabletMM} {
-		width: 99%;
-		height: 100%;
-		font-size: 1.2rem;
+    ${tabletS} {
+      width: 100%;
+      height: 100%;
+      padding: 2rem 1rem;
 
-		& > li:not(:last-child) {
-			margin-bottom: 0;
-			margin-right: 1.6rem;
-		}
-	}
+      a {
+        &.active-page {
+          filter: none;
+        }
+      }
+    }
 
-	// small
-	${tabletS} {
-		flex-direction: column;
-		letter-spacing: 0.35em;
-		font-size: 1.5rem;
-		width: 45%;
-		padding: 3rem 0;
+    // effects
+    //
+    // inverse skew on text
+    transform: var(--inverseSkewNav);
 
-		& > li:not(:last-child) {
-			margin: 0;
-		}
+    // hover effect
+    &:hover + .nav-highlight {
+      opacity: 100%;
+    }
 
-		li {
-			margin-right: 0;
-			width: 100%;
+    // active-page effect
+    &.active-page {
+      filter: invert(35%);
+    }
+  }
 
-			.nav-link-box {
-				width: 100%;
-				height: 100%;
-				padding: 2rem 0;
+  // box under link for hover effect
+  & > .nav-highlight {
+    position: absolute;
+    z-index: 0;
+    opacity: 0;
+    background: var(--main);
+    top: 0;
+    left: -2px;
+    height: 100%;
+    width: 100%;
+    transform: skew(-3deg);
+    transition: all 0.3s;
 
-				a {
-					&.active-page {
-						filter: none;
-					}
-				}
-
-				& > .nav-highlight {
-				}
-			}
-		}
-	}
-
-	${mobileMM} {
-		padding: 3.5rem 0;
-	}
+    &-1 {
+      filter: brightness(0.9);
+    }
+    &-2 {
+      filter: brightness(0.82);
+    }
+    &-3 {
+      filter: brightness(0.74);
+    }
+    &-4 {
+      filter: brightness(0.65);
+    }
+    &-5 {
+      filter: brightness(0.57);
+    }
+    &-6 {
+      filter: brightness(0.49);
+    }
+    &-7 {
+      filter: brightness(0.41);
+    }
+  }
 `
